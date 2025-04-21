fab1926a3eda1f600180c8b275040396a2ae6de6
ishikab3kb <55025206+ishikab3kb@users.noreply.github.com>
2025-04-21 21:59:52 +0530

    Time: 0 ms (100%), Space: 17.8 MB (57.36%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -19,18 +19,31 @@ class Solution:
             nums[mark]=nums[mark+1]
             nums[mark+1]=temp
             return
+        # else:
+        #     temp=nums[mark+1:]
+        #     temp.sort()
+        #     nums[mark+1:]=temp
+        #     for i in range(mark+1,n):
+        #         if(nums[mark]<nums[i]):
+        #             swaptemp=nums[mark]
+        #             nums[mark]=nums[i]
+        #             nums[i]=swaptemp
+        #             break
+        #     return
         else:
-            temp=nums[mark+1:]
-            temp.sort()
-            nums[mark+1:]=temp
-            for i in range(mark+1,n):
+            swap_idx=n-1
+            for i in range(n-1,mark,-1):
+                print(nums[i])
                 if(nums[mark]<nums[i]):
                     swaptemp=nums[mark]
                     nums[mark]=nums[i]
                     nums[i]=swaptemp
+                    swap_idx=i
                     break
+            temp_arr=nums[mark+1:]
+            temp_arr.reverse()
+            nums[mark+1:]=temp_arr
             return
-        return
 
         
 
