9d9a2f353aa4c67f3216db0a89a4964ad6903cbe
Ishika Jain <55025206+ishikab3kb@users.noreply.github.com>
2025-05-02 15:01:51 +0530

    Time: 50 ms (30.38%), Space: 61.5 MB (24.03%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.js
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.js
@@ -5,28 +5,60 @@
 var longestPalindrome = function(s) {
     if(s.length===1) return s
     else {
-        let start=0
+        
         let ans=s[0]
-        for(start=0;start<s.length-1;start++){
-            for(let end=start+1;end<s.length;end++){
-                palindromeFound=true
-                let strStart=start
-                let strEnd=end
-                while(strStart<strEnd){
-                    if(s[strStart]!==s[strEnd]){
-                        palindromeFound=false
-                        break
-                    }
-                    strStart++
-                    strEnd--
-                }
-                if(palindromeFound){
-                    let temp=s.slice(start,end+1)
-                    if(temp.length>ans.length)
-                    ans=temp
+        for(let i=1;i<s.length;i++){
+            // for(let end=start+1;end<s.length;end++){
+            //     palindromeFound=true
+            //     let strStart=start
+            //     let strEnd=end
+            //     while(strStart<strEnd){
+            //         if(s[strStart]!==s[strEnd]){
+            //             palindromeFound=false
+            //             break
+            //         }
+            //         strStart++
+            //         strEnd--
+            //     }
+            //     if(palindromeFound){
+            //         let temp=s.slice(start,end+1)
+            //         if(temp.length>ans.length)
+            //         ans=temp
                     
+            //     }
+            // }
+            let start=i-1;
+            let end=i+1;
+            while(start>=0 && end<s.length){
+                if(s[start]!==s[end]){
+                    if(ans.length<s.slice(start+1,end).length){
+                    ans=s.slice(start+1,end)
+                    console.log(ans)
+                    }
+                    break;
                 }
+                start--;
+                end++;
             }
+            if(ans.length<s.slice(start+1,end).length){
+                    ans=s.slice(start+1,end)
+                    console.log(ans)
+                    }
+            start=i-1;
+            end=i;
+            while(start>=0 && end<s.length){
+                if(s[start]!==s[end]){
+                    if(ans.length<s.slice(start+1,end).length){
+                    ans=s.slice(start+1,end)
+                    console.log(ans)
+                    }
+                    break;
+                }
+                start--;
+                end++;
+            }
+            if(ans.length<s.slice(start+1,end).length){
+                    ans=s.slice(start+1,end)}
         }
         return ans
     }
